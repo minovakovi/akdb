3d154c4440d21596b63b1bc2ebf9516f6f4238f8
RyuzakiHelix <ryuzakihelix@gmail.com>
2022-05-25 09:12:38 -0700

    changed numbers in testing() based on new tests

--- akdb/src/tests.c
+++ akdb/src/tests.c
@@ -59,6 +59,8 @@
 #include "rec/recovery.h"
 #include "sql/view.h"
 
+// NUMBERS ARE FOR COUNTING OLD IS BASED ON COMMIT FROM 2018 AND OLDER WHILE NEW IS 2022
+//COUNTED BCS THE TESTING() FUNCTION A LOT OF STUFF IS BASED ON IF(NUMBER) WHICH IS DOESNT MAKE SENSE
 Test tests[] = {
 //src/auxi:
 //---------
@@ -68,38 +70,44 @@ Test tests[] = {
 {"auxi: AK_mempro", &AK_mempro_test},//auxi/mempro.c
 {"auxi: AK_dictionary", &AK_dictionary_test},//auxi/dictionary.c
 {"auxi: AK_iniparser", &AK_iniparser_test},//auxi/iniparser.c
+//6 total
 //dm:
 //-------
 {"dm: AK_allocationbit", &AK_allocationbit_test}, //dm/dbman.c
 {"dm: AK_allocationtable", &AK_allocationtable_test}, //dm/dbman.c
 {"dm: AK_thread_safe_block_access", &AK_thread_safe_block_access_test}, //dm/dbman.c
+//3+6=9 total
 //file:
 //---------
 {"file: AK_id", &AK_id_test}, //file/id.c
 {"file: AK_lo", &AK_lo_test}, //file/blobs.c
 {"file: AK_files_test", &AK_files_test}, //file/files.c
-{"file: AK_fileio_test", &AK_fileio_test}, //file/fileio.c
-{"file: AK_op_rename", &AK_op_rename_test}, //file/table.c
+{"file: AK_fileio_test", &AK_fileio_test}, //file/fileio.c //old 10, new 13
+{"file: AK_op_rename", &AK_op_rename_test}, //file/table.c  //old 11, new 14
 {"file: AK_filesort", &AK_filesort_test}, //file/filesort.c
 {"file: AK_filesearch", &AK_filesearch_test}, //file/filesearch.c
-{"file: AK_sequence", &AK_sequence_test}, //file/sequence.c 
-{"file: AK_table_test", &AK_table_test}, //file/table.c
+{"file: AK_sequence", &AK_sequence_test}, //file/sequence.c  //old 14, new 17, old user  rinkovec  named this as btree which is not 14=btree??
+{"file: AK_table_test", &AK_table_test}, //file/table.c //old 15, new 18
+//9+9=18 total
 //file/idx:
 //-------------
 {"idx: AK_bitmap", &AK_bitmap_test}, //file/idx/bitmap.c
 {"idx: AK_btree", &AK_btree_test}, //file/idx/btree.c
 {"idx: AK_hash", &AK_hash_test}, //file/idx/hash.c
+//3+18=21 total
 //mm:
 //-------
 {"mm: AK_memoman", &AK_memoman_test}, //mm/memoman.c
 {"mm: AK_block", &AK_memoman_test2}, //mm/memoman.c
+//2+21=23 total
 //opti:
 //---------
 {"opti: AK_rel_eq_assoc", &AK_rel_eq_assoc_test}, //opti/rel_eq_assoc.c
 {"opti: AK_rel_eq_comut", &AK_rel_eq_comut_test}, //opti/rel_eq_comut.c
 {"opti: AK_rel_eq_selection", &AK_rel_eq_selection_test}, //opti/rel_eq_selection.c
 {"opti: AK_rel_eq_projection", &AK_rel_eq_projection_test}, //opti/rel_eq_projection.c
-{"opti: AK_query_optimization", &AK_query_optimization_test}, //opti/query_optimization.c
+{"opti: AK_query_optimization", &AK_query_optimization_test}, //opti/query_optimization.c //old 25, new 28
+//5+23=28 total
 //rel:
 //--------
 {"rel: AK_op_union", &AK_op_union_test}, //rel/union.c
@@ -109,32 +117,37 @@ Test tests[] = {
 {"rel: AK_op_intersect", &AK_op_intersect_test}, //rel/intersect.c
 {"rel: AK_op_selection", &AK_op_selection_test}, //rel/selection.c
 {"rel: AK_op_selection_pattern", &AK_op_selection_test_pattern}, //rel/selection.c with pattern match selections
-{"rel: AK_expression_check_test" , &AK_expression_check_test},
+//{"rel: AK_op_selection_redolog", &AK_op_selection_test_redolog}, //rel/selection.c redolog   existed in old commints, somewehre along the line removed, i returned it bcs of testing with counting tests under testing()
+{"rel: AK_expression_check_test" , &AK_expression_check_test}, //old 34, new 36
 {"rel: AK_op_difference", &AK_op_difference_test}, //rel/difference.c
 {"rel: AK_op_projection", &AK_op_projection_test}, //rel/projection.c
-{"rel: AK_op_theta_join", &AK_op_theta_join_test}, //rel/theta_join.c
+{"rel: AK_op_theta_join", &AK_op_theta_join_test}, //rel/theta_join.c //old 37, new 39
+//11+28=39 total
 //sql:
 //--------
 {"sql: AK_command", &AK_test_command}, //sql/command.c
 {"sql: AK_drop", &AK_drop_test}, //sql/drop.c
 {"sql: AK_view", &AK_view_test}, //sql/view.c
 {"sql: AK_nnull", &AK_nnull_constraint_test}, //sql/cs/nnull.c
-{"sql: AK_select", &AK_select_test}, //sql/select.c
+{"sql: AK_select", &AK_select_test}, //sql/select.c  //old 42, new 44
 {"sql: AK_trigger", &AK_trigger_test}, //sql/trigger.c
-{"sql: AK_unique", &AK_unique_test}, //sql/cs/unique.c
-{"sql: AK_function", &AK_function_test}, //sql/function.c
+{"sql: AK_unique", &AK_unique_test}, //sql/cs/unique.c //old 44, new 46
+{"sql: AK_function", &AK_function_test}, //sql/function.c //old 45, new 47
 {"sql: AK_privileges", &AK_privileges_test}, //sql/privileges.c
-{"sql: AK_reference", &AK_reference_test}, //sql/cs/reference.c
+{"sql: AK_reference", &AK_reference_test}, //sql/cs/reference.c //old 47, new 49
 {"sql: AK_constraint_between", &AK_constraint_between_test}, //sql/cs/between.c
-{"sql: AK_check_constraint", &AK_check_constraint_test}, //sql/cs/check_constraint.c
+{"sql: AK_check_constraint", &AK_check_constraint_test}, //sql/cs/check_constraint.c //old 49, new 51
 {"sql: AK_constraint_names", &AK_constraint_names_test}, //sql/cs/constraint_names.c
 {"sql: AK_insert", &AK_insert_test}, //sql/insert.c
+//14+39=53 total
 //trans:
 //----------
 {"trans: AK_transaction", &AK_test_Transaction}, //src/trans/transaction.c
+//54
 //rec:
 //----------
 {"rec: AK_recovery", &AK_recovery_test} //rec/recovery.c
+//55
 };
 //here are all tests in a order like in the folders from the github
 void help()
@@ -201,7 +214,7 @@ void choose_test()
         printf("Test: ");
         scanf("%d", &pickedTest);
         if(!pickedTest) exit( EXIT_SUCCESS );
-        while(pickedTest<0 || pickedTest>52)
+        while(pickedTest<0 || pickedTest>55)
         {
             printf("\nTest: ");
             scanf("%d", &pickedTest);
@@ -283,16 +296,25 @@ void testing (){
     while(pickedTest<allTests){
 
         
-        if (pickedTest==11||pickedTest==10)
+/* OLD VALUES ARE UP, MY NEW COUNTED VALUES ARE DOWN IF 
+MY MATH IS CORRECT BASED ON OLD USER COMMIT FROM 2018, 
+ONLY PROBLEM IS OLD USER WROTE 14 TO BE BTREE WHICH IS NOT THE CASE NOT EVEN IN OLD COUNT ???
+UPDATED OLD TO NEW BASED ON NEW TESTS ADDED, BUT IT SEEMS IT DOESNT EVEN HAVE AN IMPACT
+11,10,15 -OLD
+14,13,18 - NEW
+if (ans==14||ans==25||ans==34||ans==37||ans==42||ans==44||ans==45||ans==47||ans==49) -OLD
+if (ans==17|ans==28||ans==36||ans==39||ans==44||ans==46||ans==47||ans==49||ans==51) -NEW
+*/
+        if (pickedTest==14||pickedTest==13)
             {
                 AK_create_test_tables();
                 set_catalog_constraints();
             
             } 
-          if (pickedTest==15)
+          if (pickedTest==18)
             {
               for ( i; i < 1; i++ ) {
-                  failedTests[i] = 15; 
+                  failedTests[i] = 18; 
                }
                i++;
                 pickedTest++; //number of function
@@ -300,9 +322,9 @@ void testing (){
                 continue;
             }  
 
-             if (pickedTest==14||pickedTest==25||pickedTest==34||pickedTest==37||pickedTest==42||pickedTest==44||pickedTest==45||pickedTest==47||pickedTest==49)
+             if (pickedTest==17||pickedTest==28||pickedTest==36||pickedTest==39||pickedTest==44||pickedTest==46||pickedTest==47||pickedTest==49||pickedTest==51)
             {
-                //14 AK_btree_create -SIGSEGV
+                //14 AK_btree_create -SIGSEGV // IS THIS MISTAKE BCS RINKOVEC WROTE THEM IDK WHY
                 //25 AK_update_row_from_block -SIGSEGV
                 //34 - it works just its rly slow test
                 //37 SIGSEGV
