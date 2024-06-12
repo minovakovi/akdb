9981fe59278953a3deff2e54ec5dc15124d44305
mateja <matzunec@gmail.com>
2024-06-12 20:18:21 +0200

    Sitne dorade

--- akdb/src/sql/api_functions.h
+++ akdb/src/sql/api_functions.h
@@ -20,12 +20,31 @@ int sql_divide_int(int a, int b);
 int sql_modulo_int(int a, int b);
 int sql_power_int(int a, int b);
 
+/**
+
+ * @author Mateja Žunec
+ * @brief Returns the amount of successful and failed tests.
+ * @param successfulAmount amount of successful tests
+ * @param failedAmount amount of failed tests
+ * @return TestResult
+ */
+
+
 /* Arithmetic operations for floats */
 float sql_add_float(float a, float b);
 float sql_subtract_float(float a, float b);
 float sql_multiply_float(float a, float b);
 float sql_divide_float(float a, float b);
 float sql_power_float(float a, float b);
+
+/**
+
+ * @author Mateja Žunec
+ * @brief Returns the amount of successful and failed tests.
+ * @param successfulAmount amount of successful tests
+ * @param failedAmount amount of failed tests
+ * @return TestResult
+ */
 
 /* Standard functions */
 char* sql_now();
@@ -34,6 +53,15 @@ char* sql_left(const char *str, int n);
 char* sql_lower(char *str);
 char* sql_upper(char *str);
 char* sql_replace(char *str, char old_char, char new_char);
+
+/**
+
+ * @author Mateja Žunec
+ * @brief Returns the amount of successful and failed tests.
+ * @param successfulAmount amount of successful tests
+ * @param failedAmount amount of failed tests
+ * @return TestResult
+ */
 
 TestResult AK_api_functions_test();
 
